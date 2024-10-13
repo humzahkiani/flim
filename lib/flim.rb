079bcf4901ea1a9159298108bdea6edf0ee80f8c
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2024-10-13 00:35:49 -0400

    Cursor movement with arrow keys + virtualized console

--- lib/flim.rb
+++ lib/flim.rb
@@ -5,38 +5,59 @@ require 'debug'
 
 # Main Entrypoint
 class Flim
+  attr_accessor :virtual_console, :console
+
   def initialize(args)
     validate_args(args)
+    dimension = 30
+    execute_escape_code('?1049h')
+    self.console = IO.console # Might not fully replace $stdin
+    self.virtual_console = Array.new(dimension) { Array.new(dimension, '') }
   end
 
   def run
+    # 0. Setup
+    #   Switch to alternate buffer - \e[?1049h
+    #   Start from top, and top left (no scrollback showing)
+    #   When exiting program, return to main buffer - \e[?1049l
+    virtual_console[5] = virtual_console[5].map { '-' }
+    virtual_console[25] = virtual_console[25].map { '-' }
+
+    move_cursor(1, 1)
     # Event loop
-    off_text = 'ITS OFF'
-    on_text = 'IT BE ON'
-    curr_text = 'ITS OFF'
     loop do
       # 1. Display current window + cursor + text
       line, col = cursor_position
-      move_cursor(line, 1)
-      erase_line
-      print curr_text
+
+      # sync console to virtual console (DRAW)
+      sync_consoles(line, col)
 
       # 2. Check for input signals (keystrokes)
       response = String.new
-      $stdin.raw do |io|
+      console.raw do |io|
         response << io.readpartial(10)
       end
       key = response.scan(/(?<=\e\[).+?/).first
 
-      # Control C interrupt event loop
-      break if response == INTERRUPT
-
-      next unless key == 'C'
-
       # 3. Process input signals
+      if response == INTERRUPT
+        execute_escape_code('?1049l')
+        break
+      end
+
+      next unless MOVEMENT_KEYS.include?(key)
+
       line, col = cursor_position
-      move_cursor(line, col - 1)
-      # 4. Repeat
+      case key
+      when 'A'
+        move_cursor(line - 1, col)
+      when 'B'
+        move_cursor(line + 1, col)
+      when 'C'
+        move_cursor(line, col + 1)
+      when 'D'
+        move_cursor(line, col - 1)
+      end
     end
   end
 
@@ -45,6 +66,23 @@ class Flim
   INTERRUPT = 3.chr
   ERASE_LINE = '0K'
   CURSOR_POSITION = '6n'
+  MOVEMENT_KEYS = %w[A B C D].freeze
+
+  def sync_consoles(line_original, col_original)
+    line_start = 1
+    col_start = 1
+    virtual_console.each_with_index do |line, line_i|
+      line.each_with_index do |cell, cell_i|
+        move_cursor(line_start + line_i, col_start + cell_i)
+        print cell
+      end
+    end
+    move_cursor(line_original, col_original)
+  end
+
+  def terminal_window_size
+    IO.console.winsize
+  end
 
   def validate_args(args)
     return unless args.size > 1
