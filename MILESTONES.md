3905ae41c2dfaf154745e519b9594c0f08f9e7c5
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2025-02-09 20:17:29 -0500

    Fresh start - milestone one

---
+++ MILESTONES.md
@@ -0,0 +1,20 @@
+
+## 1. Hello World 
+- [x] Create project in github and clone down locally ✅ 2024-10-12
+- [x] Create simple ruby file that outputs "Hello World" when you run it ✅ 2024-10-12
+- [x] Set up Bundle, Gemfile, Rubocop ✅ 2024-10-12
+
+### 2. Virtual + Terminal Console - Open and Close Editor
+- [x] User can type `vim` (no file args), and open up a text editor.
+- [x] User can use `ctrl-c` to close editor
+
+### 3. Virtual + Terminal Console - Alphabet + Movement 
+- [ ] User can type words and move cursor and have it appear in editor 
+
+### 4. File Handling 
+- **Keep in mind that the File class is just a form of stream. Perhaps you can standardize the writing to terminal and file somehow?**
+- [ ] User can open a specific file, or create it if it doesn't exist by passing the filepath as an arg, ex: `flim ../my_file.txt`
+- [ ] User can save to file using `ctrl-s`
+
+
+
--- lib/flim.rb
+++ lib/flim.rb
@@ -1,170 +1,38 @@
 # frozen_string_literal: true
 
-require 'io/console'
 require 'debug'
 
-# Main Entrypoint
 class Flim
-  attr_accessor :virtual_console, :console, :file
+  attr_writer :terminal
 
-  def initialize(args)
-    validate_args(args)
-    filename = args.first
-
-    @file = create_or_find_file(filename)
-    @console = IO.console # Might not fully replace $stdin
-    @virtual_console = Array.new(DIMENSION) { String.new }
+  def initialize
+    @terminal = IO.console
   end
 
-  def run
-    # Setup
-    switch_to_alternate_buffer
-    move_cursor(1, 1)
-
-    sync_virtual_console_to_file
-    # Core event loop
-    loop do
-      line, col = cursor_position
-      sync_consoles(line, col)
-
-      response, key = detect_input_signal
-
-      process_input_signal = process_input_signal_proc
-      process_input_signal.call(response, key)
-    end
-  end
-
-  private
-
-  DIMENSION = 30
-
-  INTERRUPT = 3.chr
-  ERASE_LINE = '0K'
-  CURSOR_POSITION = '6n'
   SWITCH_TO_ALTERNATE_BUFFER = '?1049h'
   SWITCH_TO_MAIN_BUFFER = '?1049l'
-  MOVEMENT_KEYS = %w[A B C D].freeze
-
-  def update_virtual_console_char(line, col, key)
-    virtual_console.append("\n") while line > virtual_console.size
-    virtual_console[line - 1] = String.new if virtual_console[line - 1] == "\n"
-    virtual_console[line - 1] << String.new(' ') while col > virtual_console[line - 1].length
-    virtual_console[line - 1] << "\n"
-
-    virtual_console[line - 1][col - 1] = key
-  end
-
-  def sync_file_to_virtual_console
-    file.rewind
-    virtual_console.each do |line|
-      file.write(line)
-    end
-  end
-
-  def sync_virtual_console_to_file
-    self.virtual_console = file.readlines
-  end
-
-  def create_or_find_file(filename)
-    if File.exist?(filename)
-      File.open(filename, 'r+')
-    else
-      File.new(filename, 'a+')
-    end
-  end
-
-  def process_input_signal_proc
-    proc do |response, key|
-      if response == INTERRUPT
-        sync_file_to_virtual_console
-
-        file.close
-        switch_to_main_buffer
-        raise StopIteration
-      end
-
-      line, col = cursor_position
-      if response.to_s.length == key.length
-        update_virtual_console_char(line, col, key)
-        print(key)
-        next
-      end
-      next unless MOVEMENT_KEYS.include?(key)
-
-      case key
-      when 'A'
-        move_cursor(line - 1, col)
-      when 'B'
-        move_cursor(line + 1, col)
-      when 'C'
-        move_cursor(line, col + 1)
-      when 'D'
-        move_cursor(line, col - 1)
-      end
-    end
-  end
-
-  def detect_input_signal
-    response = String.new
-    console.raw do |io|
-      response << io.readpartial(10)
-    end
-    key = response.scan(/(?<=\e\[).+?/).first
-    key = response if response.to_s.length == 1
-    [response, key]
-  end
-
-  def switch_to_alternate_buffer
+  
+  # Event Loop
+  def run
+    
+    # 1. Switch to alternative buffer
     execute_escape_code(SWITCH_TO_ALTERNATE_BUFFER)
-  end
-
-  def switch_to_main_buffer
-    execute_escape_code(SWITCH_TO_MAIN_BUFFER)
-  end
-
-  def sync_consoles(line_original, col_original)
-    line_start = 1
-    col_start = 1
-    virtual_console.each_with_index do |line, line_i|
-      line_length = line.length
-      line_length.times do |col_i|
-        move_cursor(line_start + line_i, col_start + col_i)
-        char = line[col_i]
-        print char
+    loop do
+      input_sequence = String.new
+      $stdin.raw do |io|
+        buffer = io.readpartial(256) # Read a larger chunk
+        next if buffer.empty? # Exit on end of input (if spported)
+        
+        # Control C Escape Sequence
+        if buffer == "\x03"
+            exit
+        end 
       end
-    end
-    move_cursor(line_original, col_original)
-  end
-
-  def validate_args(args)
-    raise 'You may supply only one filename as an argument' if args.size != 1
+    end 
   end
 
   def execute_escape_code(code)
     print "\e[#{code}"
   end
-
-  def move_cursor(line, col)
-    move_cursor_code = "\e[#{line};#{col}H"
-    execute_escape_code(move_cursor_code)
-  end
-
-  def erase_line
-    execute_escape_code(ERASE_LINE)
-  end
-
-  # This is the cursor position relative to the viewport. It is NOT absolute. Will change on terminal scroll
-  def cursor_position
-    # Need to handle output of control sequence without echoing to terminal
-    response = String.new
-
-    # Raw mode to handle cursor position as input stream without echoing
-    console.raw do |io|
-      execute_escape_code(CURSOR_POSITION)
-
-      response << io.readpartial(1) until response[-1] == 'R'
-    end
-
-    response.scan(/\d+/).map(&:to_i)
-  end
 end
+
